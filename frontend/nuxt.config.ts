ccc33ec891d118f7d2e8c019b96ebd99dae00bf1
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-12-18 02:08:55 +0900

    ADD: tailwind css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: '2024-11-01',
+  css: ['/static/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+    },
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: http://ogp.me/ns#',
+      },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Portfolio',
+    },
+  },
   devtools: { enabled: true }
 })

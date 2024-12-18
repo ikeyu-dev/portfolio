158a59a3ecc36e8c29ded1f330b8176df0857afd
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-12-18 17:54:57 +0900

    CHANGE: ダーク/ホワイトモード切り替え系

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,37 @@
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { toggleDarkMode } from '~/composables/theme_mode';
+
+onMounted(() => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+});
+</script>
+
+<template>
+        <header class="bg-gray-50 border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <div class="mx-auto flex items-center justify-end gap-2" style="padding: 10px;">
+                <button
+                    @click="toggleDarkMode()"
+                    class="relative inline-flex items-center justify-center p-2 text-gray-500 rounded-lg hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-700"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                        class="w-6 h-6 fill-current"
+                    >
+                        <path
+                            d="M448 256c0-106-86-192-192-192l0 384c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"
+                        />
+                    </svg>
+                </button>
+            </div>
+        </header>
+
+        <slot />
+
+</template>

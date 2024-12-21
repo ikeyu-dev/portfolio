66d17d2680d729409286734f363529fa451acf61
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-12-22 02:55:27 +0900

    CHANGE: title

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
       },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'Portfolio',
+      title: 'uyuyu',
     },
   },
   runtimeConfig: {

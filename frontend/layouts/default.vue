b87f5e711772f7220d57244666699f45cc110e4e
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-12-30 16:29:29 +0900

    ADD: title logo

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -13,8 +13,13 @@ onMounted(() => {
 
 <template>
     <header
-        class="bg-gray-50 border-b border-gray-300 dark:bg-gray-800 dark:border-gray-600 transition-colors duration-300">
-        <div class="mx-auto flex items-center justify-end gap-2" style="padding: 10px;">
+        class="flex justify-between items-center bg-gray-50 border-b border-gray-300 dark:bg-gray-800 dark:border-gray-600 transition-colors duration-300 p-4">
+        <div class="flex items-center">
+            <span class="text-lg font-bold text-gray-700 dark:text-gray-300">
+                Portfolio
+            </span>
+        </div>
+        <div class="flex items-center">
             <button @click="toggleDarkMode()"
                 class="relative inline-flex items-center justify-center p-2 text-gray-500 rounded-lg hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 transition-colors duration-300">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-6 h-6 fill-current">

4d52a65ba52daacaed81f81c81c3f1d61c771c0f
uyuyuu09 <220109@oks.city-saitama.ed.jp>
2024-12-31 19:56:23 +0900

    TEST

--- frontend/server/api/get_player_data.ts
+++ frontend/server/api/get_player_data.ts
@@ -3,8 +3,6 @@ import { useRuntimeConfig } from "#imports";
 import { ref } from "vue";
 
 export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig();
-    const PLAYERS_API_URL = config.public.PLAYERS_API_URL;
     const uid = getQuery(event).uid;
     const response = ref();
     if (!uid) {
@@ -13,14 +11,17 @@ export default defineEventHandler(async (event) => {
             statusMessage: "UID is required",
         });
     }
+    const config = useRuntimeConfig();
+    const PLAYERS_API_URL = config.public.PLAYERS_API_URL;
     const url = PLAYERS_API_URL + uid;
-    try {
-        response.value = await $fetch(url);
-        return response.value;
-    } catch (e) {
-        throw createError({
-            statusCode: 500,
-            statusMessage: "Failed to fetch player data",
-        });
-    }
+    // try {
+    response.value = await $fetch(url);
+    console.log(response.value);
+    return response.value;
+    // } catch (e) {
+    //     throw createError({
+    //         statusCode: 500,
+    //         statusMessage: "Failed to fetch player data",
+    //     });
+    // }
 });

fbc81bdbae6227b44ddfc77752c246f358c5d395
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-09-20 19:58:23 +0900

    change: titleを更新

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
             },
             charset: "utf-8",
             viewport: "width=device-width, initial-scale=1",
-            title: "uyuyu",
+            title: "ikeyu-portfolio",
         },
     },
     runtimeConfig: {

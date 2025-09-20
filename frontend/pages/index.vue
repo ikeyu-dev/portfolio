ae87516af2ef6aaf0e20a98a9c5b138f52af4056
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-09-20 12:16:21 +0900

    fix: マージンとパディングの調整

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,19 +4,19 @@
             class="flex items-center justify-center"
             style="min-height: calc(100svh - 60px)"
         >
-            <div class="flex flex-col items-center py-12">
+            <div class="flex flex-col items-center">
                 <img
-                    class="w-1/3 mb-4 rounded-full shadow-lg transition-all duration-300"
+                    class="w-1/3 my-4 rounded-full shadow-lg transition-all duration-300"
                     src="https://lh3.googleusercontent.com/a/ACg8ocJyKmN00v0EKYBId9b_gms6XRo-YnT7ZbgZVYPma5cNTPhMnjo=s576-c-no"
                     alt="NO IMAGE"
                 />
                 <p
-                    class="mb-1 text-2xl font-semibold text-gray-900 transition-all duration-300"
+                    class="text-2xl font-semibold text-gray-900 transition-all duration-300"
                 >
                     Yuma Ikeda / いけゆう
                 </p>
                 <div
-                    class="mockup-code bg-gray-800 text-primary-content m-10 w-11/12"
+                    class="mockup-code bg-gray-800 text-primary-content my-4 mx-10 w-11/12"
                 >
                     <pre data-prefix="~"><code>Name?</code></pre>
                     <pre

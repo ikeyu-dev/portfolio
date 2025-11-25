c4278238d9296207f6b853dcc7306fc66e221092
Ikeda Yuma <141505741+uyuyuu09@users.noreply.github.com>
2025-11-26 02:27:46 +0900

    refactor: 不要なコードをコメントアウトし、タイトルを固定値に変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 // src/app/layout.tsx
 "use client";
 
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
+// import { useEffect, useState } from "react";
+// import { usePathname } from "next/navigation";
 import "./globals.css";
 
 export default function RootLayout({
@@ -10,16 +10,16 @@ export default function RootLayout({
 }: {
     children: React.ReactNode;
 }) {
-    const pathname = usePathname();
-    const [title, setTitle] = useState("");
+    // const pathname = usePathname();
+    // const [title, setTitle] = useState("");
 
-    useEffect(() => {
-        if (pathname === "/") {
-            setTitle("Ikeyu's Portfolio");
-        } else {
-            setTitle(pathname.split("/")[1].replaceAll("_", " "));
-        }
-    }, [pathname]);
+    // useEffect(() => {
+    //     if (pathname === "/") {
+    //         setTitle("Ikeyu's Portfolio");
+    //     } else {
+    //         setTitle(pathname.split("/")[1].replaceAll("_", " "));
+    //     }
+    // }, [pathname]);
 
     return (
         <html lang="ja">
@@ -27,7 +27,8 @@ export default function RootLayout({
                 <header className="bg-gradient-gray flex justify-between items-center transition-colors duration-300 p-4 sticky top-0 z-10">
                     <div className="flex items-center">
                         <span className="text-lg font-bold text-gray-200 transition-colors duration-300">
-                            {title}
+                            {/* {title} */}
+                            Ikeyu's Portfolio
                         </span>
                     </div>
                 </header>
